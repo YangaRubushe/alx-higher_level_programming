6dab300c02741fe5f7446e27bfa7c408b038195e
Yanga Rubushe <zilithemba10@gmail.com>
2023-12-18 13:37:22 -0800

    Task Done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+
+    except IndexError:
+        pass
     print()
-    return(total)
+    return count
