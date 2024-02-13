d0c0ba0d85576ce9c63f5645657dfc5dd171a833
Yanga Rubushe <zilithemba10@gmail.com>
2024-02-13 11:43:37 -0800

    SQL

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records of the table second_table.
+-- and also records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
